96d6f85b758afc8c0219bbf67623bb47f67cef0f
vipin yadav <cy7795151@gmail.com>
2025-01-20 20:18:10 +0530

    single or multiple dat entry syntax is written.

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ const connection = mysql.createConnection({ //establishing connection with MySQL
     password:"Xenon@56",
 });
 
-//queries
-const q = "SHOW TABLES";
+//queries to add single user data
+// const q = "INSERT INTO users (id ,username,email ,password) VALUES (?,?,?,?)";
+// let userData = ["123","123@qwe","email@123","abs"];
+
+//queries to add multiple user data.
+const q = "INSERT INTO users (id ,username,email ,password) VALUES ?";
+let userData = [
+    ["1234", "user1", "email1@example.com", "password1"],
+    ["5678", "user2", "email2@example.com", "password2"]
+  ];
 try{
-    connection.query(q,(err,result)=>{
+    connection.query(q, [userData],(err,result)=>{
         if(err) throw err;
         console.log(result);
+        console.log(result.length)
+        console.log(result[0]);
+        console.log(result[1]);
     })
 }catch(err){
     console.log(err);
