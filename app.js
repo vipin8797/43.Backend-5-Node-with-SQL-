44734cd29acbec70b4f412c59c3b5b1bf03c021d
vipin yadav <cy7795151@gmail.com>
2025-01-20 19:17:45 +0530

    faker package iss installed.

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+
+//Requiring faker
+const { faker } = require('@faker-js/faker');
+
+const getData = ()=> {
+    return {
+      userId: faker.string.uuid(),
+      username: faker.internet.username(), // before version 9.1.0, use userName()
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+  }
+
+
+  console.log(getData());
 
 
 
@@ -16,9 +31,7 @@ const app = express();
 
 
 
-
-
-const port = 3000;
-app.listen(port,()=>{
-    console.log(`listening at port ${port}`);
-})
+// const port = 3000;
+// app.listen(port,()=>{
+//     console.log(`listening at port ${port}`);
+// })
